3e69104b74f6ecae07329bba17a7c33446729935
Koki Hashimoto <gohannnotomo@gmail.com>
2009-05-07 22:31:42 +0900

    Substruction sample added. (But this is not so good sample..)

---
+++ samples/sub.rb
@@ -0,0 +1,8 @@
+require 'soundwave.rb'
+
+file1 = open("samples/wav/wav1.wav")
+w1 = Wave.new(file1)
+w2 = Wave.new("samples/wav/wav2.wav")
+new_w = w1.sub(w2)
+new_w.save('sub.wav')
+print new_w.dump
